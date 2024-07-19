309b662feae4186247889d1798da69dae667001a
Ashwin Sankar <ashwins1211@gmail.com>
2024-07-19 13:08:57 +0530

    replace masking logic with wavlm mask

--- bansuri_tts/models/speechflow.py
+++ bansuri_tts/models/speechflow.py
@@ -1,41 +1,138 @@
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, Tuple, Optional
 
 import torch
-import random
+import numpy as np
 from lightning import LightningModule
 from torchmetrics import MinMetric, MeanMetric
 
 from torchcfm.conditional_flow_matching import ExactOptimalTransportConditionalFlowMatcher
 
 
-# torch.manual_seed(42)
-def generate_mask(x, p_cond=0.5, mask_span_length=128):
-    """
-    Generate a mask for the input tensor x.
-    
-    Parameters:
-    - x (Tensor): Input tensor of shape (num_frames, num_features).
-    - p_cond (float): Probability of applying the mask to a frame.
-    - mask_span_length (int): Minimum span length of frames to mask.
-    
-    Returns:
-    - mask (Tensor): Mask tensor of shape (num_frames, num_features), with 1s indicating masked positions and 0s indicating unmasked positions.
-    """
-    batch_size, num_frames, num_features = x.size()
-    mask = torch.ones(batch_size, num_features, num_frames).to(device=x.device, dtype=torch.int16)
+def compute_mask_indices(
+    shape: Tuple[int, int],
+    padding_mask: Optional[torch.Tensor],
+    mask_prob: float,
+    mask_length: int,
+    mask_type: str = "static",
+    mask_other: float = 0.0,
+    min_masks: int = 0,
+    no_overlap: bool = False,
+    min_space: int = 0
+) -> torch.tensor:
+    """ Adapted from WavLM
+    Computes random mask spans for a given shape
 
-    j = 0
-    for i in range(batch_size):
-      while j < num_frames:
-          if  random.random() > p_cond:
-              start_pos = max(0, j - mask_span_length // 2)
-              end_pos = min(num_frames, j + mask_span_length // 2)
-              mask[i, :, start_pos:end_pos] = 0
-              j = end_pos
-          else:
-              j += mask_span_length // 2
+    Args:
+        shape: the the shape for which to compute masks.
+            should be of size 2 where first element is batch size and 2nd is timesteps
+        padding_mask: optional padding mask of the same size as shape, which will prevent masking padded elements
+        mask_prob: probability for each token to be chosen as start of the span to be masked. this will be multiplied by
+            number of timesteps divided by length of mask span to mask approximately this percentage of all elements.
+            however due to overlaps, the actual number will be smaller (unless no_overlap is True)
+        mask_type: how to compute mask lengths
+            static = fixed size
+            uniform = sample from uniform distribution [mask_other, mask_length*2]
+            normal = sample from normal distribution with mean mask_length and stdev mask_other. mask is min 1 element
+            poisson = sample from possion distribution with lambda = mask length
+        min_masks: minimum number of masked spans
+        no_overlap: if false, will switch to an alternative recursive algorithm that prevents spans from overlapping
+        min_space: only used if no_overlap is True, this is how many elements to keep unmasked between spans
+    """
 
-    return mask
+    bsz, all_sz = shape
+    mask = np.full((bsz, all_sz), False)
+
+    all_num_mask = int(
+        # add a random number for probabilistic rounding
+        mask_prob * all_sz / float(mask_length)
+        + np.random.rand()
+    )
+
+    all_num_mask = max(min_masks, all_num_mask)
+
+    mask_idcs = []
+    for i in range(bsz):
+        if padding_mask is not None:
+            sz = all_sz - padding_mask[i].long().sum().item()
+            num_mask = int(
+                # add a random number for probabilistic rounding
+                mask_prob * sz / float(mask_length)
+                + np.random.rand()
+            )
+            num_mask = max(min_masks, num_mask)
+        else:
+            sz = all_sz
+            num_mask = all_num_mask
+
+        if mask_type == "static":
+            lengths = np.full(num_mask, mask_length)
+        elif mask_type == "uniform":
+            lengths = np.random.randint(mask_other, mask_length * 2 + 1, size=num_mask)
+        elif mask_type == "normal":
+            lengths = np.random.normal(mask_length, mask_other, size=num_mask)
+            lengths = [max(1, int(round(x))) for x in lengths]
+        elif mask_type == "poisson":
+            lengths = np.random.poisson(mask_length, size=num_mask)
+            lengths = [int(round(x)) for x in lengths]
+        else:
+            raise Exception("unknown mask selection " + mask_type)
+
+        if sum(lengths) == 0:
+            lengths[0] = min(mask_length, sz - 1)
+
+        if no_overlap:
+            mask_idc = []
+
+            def arrange(s, e, length, keep_length):
+                span_start = np.random.randint(s, e - length)
+                mask_idc.extend(span_start + i for i in range(length))
+
+                new_parts = []
+                if span_start - s - min_space >= keep_length:
+                    new_parts.append((s, span_start - min_space + 1))
+                if e - span_start - keep_length - min_space > keep_length:
+                    new_parts.append((span_start + length + min_space, e))
+                return new_parts
+
+            parts = [(0, sz)]
+            min_length = min(lengths)
+            for length in sorted(lengths, reverse=True):
+                lens = np.fromiter(
+                    (e - s if e - s >= length + min_space else 0 for s, e in parts),
+                    np.int,
+                )
+                l_sum = np.sum(lens)
+                if l_sum == 0:
+                    break
+                probs = lens / np.sum(lens)
+                c = np.random.choice(len(parts), p=probs)
+                s, e = parts.pop(c)
+                parts.extend(arrange(s, e, length, min_length))
+            mask_idc = np.asarray(mask_idc)
+        else:
+            min_len = min(lengths)
+            if sz - min_len <= num_mask:
+                min_len = sz - num_mask - 1
+
+            mask_idc = np.random.choice(sz - min_len, num_mask, replace=False)
+
+            mask_idc = np.asarray(
+                [
+                    mask_idc[j] + offset
+                    for j in range(len(mask_idc))
+                    for offset in range(lengths[j])
+                ]
+            )
+
+        mask_idcs.append(np.unique(mask_idc[mask_idc < sz]))
+
+    min_len = min([len(m) for m in mask_idcs])
+    for i, mask_idc in enumerate(mask_idcs):
+        if len(mask_idc) > min_len:
+            mask_idc = np.random.choice(mask_idc, min_len, replace=False)
+        mask[i, mask_idc] = True
+
+    return torch.from_numpy(mask)
 
 def mask_from_lens(seq_lengths, device=None):
     """
@@ -110,7 +207,7 @@ class SpeechFlow(LightningModule):
         self.val_loss_best.reset()
 
     def model_step(
-        self, batch: Tuple[torch.Tensor, torch.Tensor]
+        self, batch: Dict[str, torch.Tensor]
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         """Perform a single model step on a batch of data.
 
@@ -121,16 +218,26 @@ class SpeechFlow(LightningModule):
             - A tensor of predictions.
             - A tensor of target labels.
         """
-        x1, seq_lens = batch["audio_signals"].signal, batch["audio_lens"]
+        # prep data
+        x1, seq_lens = batch["mel_spec"], batch["mel_spec_lens"]
         y = x1.clone()
-        mask = mask_from_lens(seq_lens, device=x1.device)
-        x0 = torch.randn_like(x1).to(x1)
+        # generate length mask and random content mask
+        len_mask = mask_from_lens(seq_lens, device=x1.device)
+        x_mask = compute_mask_indices((x1.size(0), x1.size(2)), len_mask, 0.85, 128).to(x1.device)
+        x1 = x1 * x_mask
+        # model step
+        x0 = torch.randn_like(x1)
         t, xt, ut, _, y1 = self.FM.guided_sample_location_and_conditional_flow(x0, x1, y1=y)
-        logits = self.forward(xt, t, y, mask)
+        logits = self.forward(xt, t, y, len_mask)
+        # neg mask to compute loss for MLM
+        neg_x_mask = (~x_mask) * len_mask
+        logits = logits * neg_x_mask
+        y = y * neg_x_mask
+        # loss computation
         loss = self.criterion(logits, y)
         return loss, logits, y
     
-    def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> torch.tensor:
+    def training_step(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> torch.tensor:
         """Perform a single training step on a batch of data from the training set.
 
         :param batch: A batch of data (a tuple) containing the input tensor of images and target
@@ -148,7 +255,7 @@ class SpeechFlow(LightningModule):
 
         return loss
     
-    def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> None:
+    def validation_step(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> None:
         """Perform a single validation step on a batch of data from the validation set.
 
         :param batch: A batch of data (a tuple) containing the input tensor of images and target
@@ -163,7 +270,7 @@ class SpeechFlow(LightningModule):
         self.log("val/loss", self.val_loss, on_step=False, on_epoch=True, prog_bar=True)
         # self.log("val/acc", self.val_acc, on_step=False, on_epoch=True, prog_bar=True)
 
-    def test_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> None:
+    def test_step(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> None:
         """Perform a single test step on a batch of data from the test set.
 
         :param batch: A batch of data (a tuple) containing the input tensor of images and target
